ec979766981c822fb290b74e71a8a472d050f46b
syr1tsa <syr1tsa9@yandex.ru>
2020-08-23 01:51:41 +0300

    inital commit

---
+++ app/models/user.rb
@@ -0,0 +1,59 @@
+require 'openssl'
+
+class User < ApplicationRecord
+
+  ITERATIONS = 2000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+  validates :email, email: true
+  validates :username, format: { with: /\A[a-zA-Z&_]+\z/,
+                                 length: { maximum: 40 },
+                                 message: "only allows letters" }
+
+  attr_accessor :password
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
+  before_create :validate_username
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  def validate_username
+    username = self.username.downcase
+    self.username = nil
+    self.username = username unless User.all.map(&:username).include? username
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    return nil unless user.present?
+
+    hashed_password = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(
+        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+      )
+    )
+
+    return user if user.password_hash == hashed_password
+
+    nil
+  end
+end
