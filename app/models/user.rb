d7f1c53fb670ec91f6ba8e09899a9a6c9fcef7fe
syr1tsa <syr1tsa9@yandex.ru>
2020-08-30 00:11:11 +0300

    fixed user class and changed method to_lower_case

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,31 +10,13 @@ class User < ApplicationRecord
 
   has_many :questions
 
-  validates :email, email: true, presence: true, uniqueness: true
   validates :username, format: { with: REGULAR_FOR_USERNAME }, presence: true, uniqueness: true
   validates_length_of :username, maximum: 40
+  validates :email, email: true, presence: true, uniqueness: true
+  validates :password, on: :create, presence: true, confirmation: true
 
-  validates_presence_of :password, on: :create
-  validates_confirmation_of :password
-
-  before_validation :username_to_lower_case
+  before_validation :to_lower_case
   before_save :encrypt_password
-
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
-
-  def username_to_lower_case
-    self.username = self.username.downcase
-  end
-
-  private
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -55,4 +37,24 @@ class User < ApplicationRecord
 
     nil
   end
+
+  private
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  def to_lower_case
+    unless self.username.nil? & self.email.nil?
+      self.username = self.username.downcase
+      self.email = self.email.downcase
+    end
+  end
+
 end
