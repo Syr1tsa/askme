d2e8a3c72039382962f1bec7e05cdf2128d1194c
syr1tsa <syr1tsa9@yandex.ru>
2020-08-30 00:23:16 +0300

    added user-questions-lenght block for user.show.html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def get_word_form(number, form_1, form_2, form_3)
+    number = number.abs
+    remainder = number % 10
+    last_num_in_number = number % 100
+
+    puts remainder.to_s
+
+    return form_1 if remainder == 1 && !(last_num_in_number.between?(11,14))
+    return form_2 if (2..4).include?remainder && !(last_num_in_number.between?(11,14))
+    return form_3 if remainder.between?(5,14) || last_num_in_number.between?(11,14) || remainder == 0
+  end
 end
