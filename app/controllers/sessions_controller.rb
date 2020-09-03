daabdf367ad71ae5b912eb8b301e19de3c0c4c36
syr1tsa <syr1tsa9@yandex.ru>
2020-09-03 22:00:21 +0300

    fixed controllers user & sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.authenticate(params[:email], params[:password])
+    user = User.authenticate(params[:email], params[:password])
 
-    if @user.present?
-      session[:user_id] = @user.id
+    if user.present?
+      session[:user_id] = user.id
       redirect_to root_url, notice: "#{@user.name}, вы успешно вошл на сайт Ask me :)"
     else
       flash.now.alert = "Неверный email или пароль"
