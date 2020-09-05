9f604a5bc18aef229076ee7d87a99caaaf2517e0
syr1tsa <syr1tsa9@yandex.ru>
2020-09-06 02:48:03 +0300

    Added user.username for user.index.view

--- db/migrate/20200904230926_delete_index_username_email_lower.rb
+++
@@ -1,5 +0,0 @@
-class DeleteIndexUsernameEmailLower < ActiveRecord::Migration[6.0]
-  def change
-    remove_index ['lower(users)', 'lower(email)'], name: "index_lower_users_on_lower_email_index"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_173701) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "username"!
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
