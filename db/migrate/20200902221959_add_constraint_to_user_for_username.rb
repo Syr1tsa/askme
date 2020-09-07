e19069f73ff26b64f6069c65e4b17f8e0d61e103
syr1tsa <syr1tsa9@yandex.ru>
2020-09-07 22:23:19 +0300

    delete migration

--- db/migrate/20200902221959_add_constraint_to_user_for_username.rb
+++
@@ -1,6 +0,0 @@
-class AddConstraintToUserForUsername < ActiveRecord::Migration[6.0]
-  def change
-    add_index ['lower(users)'], ['lower(email)'], :name => "index_lower_users_on_lower_email_index", :unique => true
-  end
-end
-  
