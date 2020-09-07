5be815c5e26f6ebf552b6f823dc9ca41cca83052
syr1tsa <syr1tsa9@yandex.ru>
2020-09-07 23:07:07 +0300

    delete helper for avatar image. Added regex for user.avatar_url and set default

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   REGULAR_FOR_USERNAME = /\A[a-z_\d]+\z/
   REGULAR_FOR_NAME = /\A[a-zA-Z]+\z/
+  REGULAR_FOR_AVATAR_URL = /\A((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?)|avatar.jpg\z/ix
 
   attr_accessor :password
 
@@ -15,8 +16,9 @@ class User < ApplicationRecord
   validates :username, format: { with: REGULAR_FOR_USERNAME }, length: { maximum: 40 }, presence: true, uniqueness: true
   validates :email, email: true, presence: true, uniqueness: true
   validates :password, on: :create, presence: true, confirmation: true
+  validates_format_of :avatar_url, with: REGULAR_FOR_AVATAR_URL
 
-  before_validation :to_lower_case
+  before_validation :to_lower_case, :default_image_url
   before_save :encrypt_password
 
   def self.hash_to_string(password_hash)
@@ -48,6 +50,10 @@ class User < ApplicationRecord
         OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
       )
     end
+  end
+
+  def default_image_url
+    self.avatar_url = 'avatar.jpg' if avatar_url.nil?
   end
 
   def to_lower_case
