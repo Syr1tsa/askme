10670f9df9b3bf3bb3644c736a89cd2e5f1d3b18
syr1tsa <syr1tsa9@yandex.ru>
2020-09-15 01:15:44 +0300

    fixed config i18n

--- config/application.rb
+++ config/application.rb
@@ -21,16 +21,12 @@ Bundler.require(*Rails.groups)
 
 module Askme
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    config.time_zone = 'Moscow'
-
     config.generators.system_tests = nil
 
-    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
+    config.i18n.default_locale = :en
     config.i18n.locale = :ru
-    config.i18n.available_locales = %i[en ru]
 
     config.i18n.fallbacks = [:en]
   end
